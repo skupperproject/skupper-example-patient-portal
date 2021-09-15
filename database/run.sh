abb45986dde34e04d575c22ff98b7a18c831c45e
Justin Ross <jross@apache.org>
2021-09-15 16:41:25 -0400

    WIP

--- database/run.sh
+++ database/run.sh
@@ -1,24 +1,3 @@
 #!/bin/bash
 
-dir=$(dirname "$0")
-
-podman run --rm --name patient-portal-database --detach --net host \
-       -e POSTGRES_USER=patient_portal \
-       -e POSTGRES_PASSWORD=secret \
-       -e POSTGRES_HOST_AUTH_METHOD=scram-sha-256 \
-       -e POSTGRES_INITDB_ARGS=--auth-host=scram-sha-256 \
-       docker.io/postgres
-
-trap "podman kill patient-portal-database" EXIT
-
-echo "Waiting for the database to start up"
-
-sleep 10
-
-echo "Loading data"
-
-psql postgresql://patient_portal:secret@localhost/patient_portal < $dir/data.sql
-
-echo "Following the logs"
-
-podman logs --follow patient-portal-database
+exec podman run --net host quay.io/ssorj/patient-portal-database
